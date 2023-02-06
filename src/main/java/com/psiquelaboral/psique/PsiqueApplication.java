dd915548db8dd76c82b7c030f9d1475e699c69d9
Joaquin Coronado <joaquincora@hotmail.com>
2023-02-05 21:10:40 -0600

    first commit

---
+++ src/main/java/com/psiquelaboral/psique/PsiqueApplication.java
@@ -0,0 +1,13 @@
+package com.psiquelaboral.psique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiqueApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PsiqueApplication.class, args);
+  }
+
+}
