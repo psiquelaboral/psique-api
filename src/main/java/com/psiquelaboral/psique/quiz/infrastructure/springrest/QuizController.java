8f880745db6dc70e225d8bd1893c54b6c24b1679
Joaquin Coronado <joaquincora@hotmail.com>
2023-03-23 22:15:50 -0600

    Add quiz controller

---
+++ src/main/java/com/psiquelaboral/psique/quiz/infrastructure/springrest/QuizController.java
@@ -0,0 +1,10 @@
+package com.psiquelaboral.psique.quiz.infrastructure.springrest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class QuizController {
+    
+}
