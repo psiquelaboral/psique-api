a6430c21805c5f24ec754e7067af48e2a5ba6067
Joaquin Coronado <joaquincora@hotmail.com>
2023-05-08 00:02:18 -0600

    fix authentication

--- src/main/java/com/psiquelaboral/psique/auth/infrastructure/springrest/AuthController.java
+++ src/main/java/com/psiquelaboral/psique/auth/infrastructure/springrest/AuthController.java
@@ -8,22 +8,27 @@ import com.psiquelaboral.psique.user.domain.model.PsiqueUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("auth/")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationManager authManager;
     private final IPsiqueUserService userService;
     private final JWTService jwtService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+
+        // Make the authentication with BCrypt
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
 
         //Get the current user
         PsiqueUser authenticatedUser = this.userService.getByEmail(authRequest.getEmail());
