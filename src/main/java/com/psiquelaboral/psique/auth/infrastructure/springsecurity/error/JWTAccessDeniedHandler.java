e8da06b771b01f5b2a03be250242df442753805f
Joaquin Coronado <joaquincora@hotmail.com>
2024-02-29 21:08:26 -0600

    Update object building on accessDeniedHandler and AuthenticationEntryPoint

--- src/main/java/com/psiquelaboral/psique/auth/infrastructure/springsecurity/error/JWTAccessDeniedHandler.java
+++ src/main/java/com/psiquelaboral/psique/auth/infrastructure/springsecurity/error/JWTAccessDeniedHandler.java
@@ -5,13 +5,13 @@ import com.psiquelaboral.psique.auth.domain.model.AuthErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Component
 public class JWTAccessDeniedHandler implements AccessDeniedHandler {
@@ -22,20 +22,18 @@ public class JWTAccessDeniedHandler implements AccessDeniedHandler {
 
         // Set 403 http code to response and add JSON as content type
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
 
         //Create AuthError response with models
         AuthErrorResponse error = AuthErrorResponse.builder()
-                .title("Forbidden")
-                .status(403)
-                .details(accessDeniedException.getMessage())
-                .build();
+            .title("Forbidden")
+            .status(403)
+            .details(accessDeniedException.getMessage())
+            .build();
 
         //Write the response with JSON format
-        OutputStream out = response.getOutputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(out, error);
-        out.flush();
+        String errorAsJson = new ObjectMapper().writeValueAsString(error);
+        response.getWriter().write(errorAsJson);
     }
 
 }
--- src/main/java/com/psiquelaboral/psique/auth/infrastructure/springsecurity/error/JWTAuthenticationEntryPoint.java
+++ src/main/java/com/psiquelaboral/psique/auth/infrastructure/springsecurity/error/JWTAuthenticationEntryPoint.java
@@ -5,13 +5,13 @@ import com.psiquelaboral.psique.auth.domain.model.AuthErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 
 @Component
@@ -23,20 +23,18 @@ public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
         // Set 401 http code to response and add JSON as content type
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
         //Create AuthError response with models
         AuthErrorResponse error = AuthErrorResponse.builder()
-                .title("Unauthorized")
-                .status(401)
-                .details(authException.getMessage())
-                .build();
+            .title("Unauthorized")
+            .status(401)
+            .details(authException.getMessage())
+            .build();
 
         //Write the response with JSON format
-        OutputStream out = response.getOutputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(out, error);
-        out.flush();
+        String errorAsJson = new ObjectMapper().writeValueAsString(error);
+        response.getWriter().write(errorAsJson);
     }
 
 }
